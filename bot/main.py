cad0a294d17cdfcc9dce5f7c7b76ae392baabe26
A3incSTIGMAT <brilsash@yandex.ru>
2025-01-19 16:16:35 +0800

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,1 +1,13 @@
+from aiogram import Bot, Dispatcher
+from bot.handlers import start  # Импортируем обработчики
+
+BOT_TOKEN = "ВашТокен"
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+dp.include_router(start.router)
+
+if __name__ == "__main__":
+    print("Бот успешно запущен!")
 
