eb5d2f4ac94c41419aa403f433e034327a0e8db7
A3incSTIGMAT <brilsash@yandex.ru>
2025-01-20 14:50:06 +0800

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -3,6 +3,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.types import Update
 from dotenv import load_dotenv
 import os
+import logging
 
 # Загружаем переменные окружения из .env файла
 load_dotenv()
@@ -12,11 +13,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 PORT = os.getenv("PORT", 8000)
 
-# Проверяем, были ли загружены переменные
-print(f"BOT_TOKEN: {BOT_TOKEN}")
-print(f"WEBHOOK_URL: {WEBHOOK_URL}")
-
-# Если переменные не заданы, выводим ошибку
+# Проверка наличия BOT_TOKEN и WEBHOOK_URL
 if not BOT_TOKEN or not WEBHOOK_URL:
     raise ValueError("BOT_TOKEN или WEBHOOK_URL не указаны в .env файле.")
 
@@ -26,17 +23,28 @@ bot = Bot(token=BOT_TOKEN)
 # Создаем диспетчер с аргументом bot
 dp = Dispatcher(bot=bot)
 
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 # Функция для обработки старта приложения
 async def on_startup(app):
     # Устанавливаем webhook
+    logger.info(f"Setting webhook with URL: {WEBHOOK_URL}")
     await bot.set_webhook(WEBHOOK_URL)
 
 # Обработчик для получения webhook-запросов
 async def webhook(request):
-    json_str = await request.json()  # Получаем данные запроса
-    update = Update(**json_str)  # Преобразуем их в объект Update
-    await dp.process_update(update)  # Обрабатываем обновление
-    return web.Response(status=200)  # Ответ
+    try:
+        json_str = await request.json()  # Получаем данные запроса
+        update = Update(**json_str)  # Преобразуем их в объект Update
+        await dp.process_update(update)  # Обрабатываем обновление
+
+        logger.info(f"Webhook received: {json_str}")  # Логирование принятого запроса
+        return web.Response(status=200)  # Ответ
+    except Exception as e:
+        logger.error(f"Error processing webhook: {e}")
+        return web.Response(status=500)  # Ответ в случае ошибки
 
 # Создаем приложение aiohttp
 app = web.Application()
@@ -49,7 +57,9 @@ app.router.add_post('/webhook', webhook)
 
 # Запуск приложения
 if __name__ == '__main__':
+    logger.info(f"Starting server on port {PORT}...")
     web.run_app(app, host='0.0.0.0', port=int(PORT))  # Запускаем сервер
+
 
 
 
