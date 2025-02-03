8bbac506426e87a62b0fb00406c961c3fc7ebf22
A3incSTIGMAT <brilsash@yandex.ru>
2025-02-03 18:18:13 +0800

    Update __init__.py

--- bot/database/__init__.py
+++ bot/database/__init__.py
@@ -1,11 +1,10 @@
-# bot/database/__init__.py
+from .crud import DatabaseManager  # Импорт класса из crud.py
 from .engine import create_async_engine, get_session
 from .models import Base
-from .crud import DatabaseManager
 
 __all__ = [
+    "DatabaseManager",
     "create_async_engine",
     "get_session",
-    "Base",
-    "DatabaseManager"
+    "Base"
 ]
